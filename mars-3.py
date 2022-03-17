7f8e5d9580ef29675cbaed8407a29ca1ccaedcfe
mariammagued <mariam.magued24@gmail.com>
2022-03-17 17:49:27 +0200

    added LPF

--- mars-3.py
+++ mars-3.py
@@ -9,16 +9,14 @@ from skimage.io import imread    # pip install scikit-image
 #########################################
 # SHOW PLOT FUNCTION
 #########################################
-
 def showPlot(img):
     plt.figure(figsize=(10, 10))
     plt.imshow(img)
     plt.axis('off')
     # plt.show()
 
-
 #########################################
-# HIGH PASS FILTER
+# MOVE IMAGE TO FREQUENCY DOMAIN
 #########################################
 def toFFT(img):
     F1 = fp.fft2((img).astype(float))
@@ -26,11 +24,32 @@ def toFFT(img):
     fft = (20*np.log10(0.1 + F2)).astype(int)
     return F1, F2, fft
 
+
+#########################################
+# HIGH-PASS FILTER
+#########################################
 def highPassFiltering(img, F2):
     (w, h) = img.shape
     half_w, half_h = int(w/2), int(h/2)
-    n = 50    # high pass size
-    F2[half_w-n:half_w+n+1,half_h-n:half_h+n+1] = 0    # select all but the first 50x50 (low) frequencies
+    n = 50    # high-pass size
+    # select all but the first 50x50 frequencies
+    F2[half_w-n:half_w+n+1, half_h-n:half_h+n+1] = 0
+    fft = (20*np.log10(0.1 + F2)).astype(int)
+    img = fp.ifft2(fp.ifftshift(F2)).real
+    return img, fft, F2
+
+
+#########################################
+# LOW-PASS FILTER
+#########################################
+def lowPassFiltering(img, F2):
+    (w, h) = img.shape
+    half_w, half_h = int(w/2), int(h/2)
+    n = 30    # low-pass size
+    Fblank = np.zeros((w, h), np.uint8)
+    # select the first 30x30 frequencies
+    Fblank[half_w-n:half_w+n+1, half_h-n:half_h+n+1] = 1
+    F2 = Fblank*F2
     fft = (20*np.log10(0.1 + F2)).astype(int)
     img = fp.ifft2(fp.ifftshift(F2)).real
     return img, fft, F2
@@ -49,14 +68,15 @@ imgOG = cv2.cvtColor(imgOG, cv2.COLOR_BGR2RGB)
 # showPlot(imgOG)
 
 # get v-channel from HSV format to apply filter
-imgValues = imgFiltered[:,:,2]
+imgValues = imgFiltered[:, :, 2]
 
 # move to frequency domain then apply highpass filter
 F1, F2, fftOG = toFFT(imgValues)
-imgValues, fftHP, F2 = highPassFiltering(imgValues, F2)
+# imgValues, fftHP, F2 = highPassFiltering(imgValues, F2)
+imgValues, fftFilter, F2 = lowPassFiltering(imgValues, F2)
 
 # overwrite v-vhannel in filtered image variable
-imgFiltered[:,:,2] = imgValues
+imgFiltered[:, :, 2] = imgValues
 imgFiltered = cv2.cvtColor(imgFiltered, cv2.COLOR_HSV2RGB)
 # showPlot(imgFiltered)
 
@@ -65,8 +85,8 @@ plt.axis('off')
 plt.subplot(222), plt.imshow(fftOG, 'gray'), plt.title('original freq')
 plt.axis('off')
 plt.subplot(223), plt.imshow(imgFiltered), plt.title('Effect after filtering')
-plt.subplot(224), plt.imshow(fftHP, 'gray'), plt.title('High pass filter')
 plt.axis('off')
+plt.subplot(224), plt.imshow(fftFilter, 'gray'), plt.title('filter frequency')
 plt.axis('off')
 
 
